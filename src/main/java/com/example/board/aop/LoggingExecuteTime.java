0529ef0a185e75c2a2e706433129367011363989
taetano <taetano96@gmail.com>
2023-11-06 16:20:14 +0900

    🩹 aop범위 Controller로 설정

--- src/main/java/com/example/board/aop/LoggingExecuteTime.java
+++ src/main/java/com/example/board/aop/LoggingExecuteTime.java
@@ -7,22 +7,25 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Log4j2
 @Aspect
 @Component
 public class LoggingExecuteTime {
-    @Pointcut("(execution(* com.example.board.*.*(..) ))")
-    public void loggingPointcut() {};
+    @Pointcut("(execution(* com.example.board.*.*Controller.*(..) ))")
+    public void loggingPointcut() {}
 
     @Around("loggingPointcut()")
     public Object loggingExecution(ProceedingJoinPoint pjp) throws Throwable {
         long startTime = System.currentTimeMillis();
 
         Object result = pjp.proceed();
+        Arrays.stream(pjp.getArgs()).forEach(arg -> log.info("{} ", arg));
 
         long endTime = System.currentTimeMillis();
 
-        log.info("execution time {}", startTime - endTime);
+        log.info("{}: execution time {}", pjp.getSignature(),startTime - endTime);
 
         return result;
     }
